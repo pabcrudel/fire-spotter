314b38b3847bd205107213308bb412c88c05f569
pabcrudel <pabcrudel@gmail.com>
2023-12-03 21:19:31 +0100

    add: Refine Vite configuration to adapt to a modified root path

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import glob from 'fast-glob';
+import path from 'path';
+
+/** Specify which is the source folder name */
+const sourceFolder = 'src';
+
+export default defineConfig({
+  base: './',
+  root: sourceFolder,
+  build: {
+    /* Because of `root path` has changed, `ourDir` and `emptyOutDir` must be
+    * specify.
+    * - `ourDir`: is relative to `root path`
+    * - `emptyOutDir`: true === override
+    */
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: glob.sync(`./${sourceFolder}/**/*.html`).map(
+        entry => path.resolve(__dirname, entry)
+      )
+    },
+  },
+})
